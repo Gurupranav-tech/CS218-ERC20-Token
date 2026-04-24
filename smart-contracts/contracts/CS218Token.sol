52013db9f7d7d21882fcd5455d6f464c9b2f087c
Gurupranav-tech <gurupranav08@gmail.com>
2026-04-24 18:03:51 +0000

    Added coverage reports

--- smart-contracts/contracts/CS218Token.sol
+++ smart-contracts/contracts/CS218Token.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol"
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CS218Token is ERC20, Ownable {
     address public minter;
